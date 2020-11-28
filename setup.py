736c64d4f43b6b93024cfaf85db01922b5bcca51
Franco Ruggeri <fruggeri@kth.se>
2020-11-28 02:18:07 +0100

    update report and package info

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='dualstudent-asr',
-    version='0.1.0',
-    description='Dual Student training for Automatic Speech Recognition',
-    author='Andrea Caraffa, Kevin Dalla Torre Castillo, Simone Porcu, Franco Ruggeri',
-    author_email='caraffa@kth.se, kevindt@kth.se, porcu@kth.se, fruggeri@kth.se',
+    version='0.1.2',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    description='Dual Student for Automatic Speech Recognition',
+    author='Franco Ruggeri, Andrea Caraffa, Kevin Dalla Torre Castillo, Simone Porcu',
+    author_email='fruggeri@kth.se, caraffa@kth.se, kevindt@kth.se, porcu@kth.se',
     license='GPL',
     packages=find_packages(include=['dualstudent', 'dualstudent.*']),
-    include_package_data=True
+    include_package_data=True,
+    url='https://github.com/franco-ruggeri/dt2119-dualstudent-asr'
 )
